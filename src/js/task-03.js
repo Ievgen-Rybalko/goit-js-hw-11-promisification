dbc0287da1b858154ccd8f53f5389791c927c9c7
Ievgen-Rybalko <jackrjack1@gmail.com>
2021-06-05 02:57:58 +0300

    fix: time

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -10,7 +10,8 @@ const makeTransaction = (transaction) => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id, delay);
+        
+        resolve( [transaction.id, delay] );
       } else {
         reject(transaction.id);
       }
@@ -20,7 +21,8 @@ const makeTransaction = (transaction) => {
 };
 
 
-const logSuccess = (id, time) => {
+
+const logSuccess = ( [id, time] ) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
